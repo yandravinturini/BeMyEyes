3e1bee718ee09d888267e1219532da8592a0efbc
Yandra Vinturini <yandravinturini@gmail.com>
2022-11-07 00:06:23 +0000

    styling dropdown, ícones e containers na página de perfil

--- screens/ProfilePage.tsx
+++ screens/ProfilePage.tsx
@@ -1,60 +1,67 @@
 import { StyleSheet, Text, View, TextInput, Image, Pressable } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
-// import userImage from '../assets/images/Ellipse 2.png'  source={userImage};
+import userImage from '../assets/images/foto_perfil.png';
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
-
 export function Profile() {
 
   const [age, setAge] = useState();
 
   return (
     <View style={styles.container}>
-
       <View style={styles.userImgContainer}>
-        <Image source={require('../assets/images/foto_perfil.png')}/>
-        <Ionicons name="camera-outline" style={styles.cameraIcon} />
+        <View style={{ width: '33%' }}></View>
+        <View style={{ width: '33%' }}>
+          <Image source={userImage} style={styles.imagem} />
+          {/* <Image source={require('../assets/images/foto_perfil.png')} /> */}
+          <Ionicons name="camera-outline" style={styles.cameraIcon} />
+        </View>
+        <View style={{ width: '33%' }}></View>
       </View>
+      <View style={styles.formContainer}>
+        <Text style={styles.text}>Username</Text>
 
-      <Text style={styles.text}>Username</Text>
+        <TextInput style={styles.input}
+          // onChangeText={onChangeNumber}
+          // value={number}
+          placeholder="maria" />
 
-      <TextInput style={styles.input}
-        // onChangeText={onChangeNumber}
-        // value={number}
-        placeholder="maria" />
+        <Text style={styles.text}>Age</Text>
+        <View style={styles.dropdownContainer}>
+          <Picker style={{
+            height: 20,
+            width: 120
+          }}
+            itemStyle={styles.dropdown}
+            selectedValue={age}
+            onValueChange={(itemValue, itemIndex) =>
+              setAge(itemValue)
+            }>
 
-      <Text style={styles.text}>Age</Text>
+            <Picker.Item label="18-25" value="jovem" />
+            <Picker.Item label="26-30" value="jovem-adulto" />
+            <Picker.Item label="31-65" value="adulto" />
+            <Picker.Item label="66+" value="senior" />
+          </Picker>
+        </View>
+        <Text style={styles.text}>Email</Text>
 
-      <Picker style={styles.dropdown}
-        selectedValue={age}
-        onValueChange={(itemValue, itemIndex) =>
-          setAge(itemValue)
-        }>
+        <TextInput style={styles.input}
+          // onChangeText={onChangeNumber}
+          // value={number}
+          placeholder="maria@gmail.com" />
 
-        <Picker.Item label="18-25" value="jovem" />
-        <Picker.Item label="26-30" value="jovem-adulto" />
-        <Picker.Item label="31-65" value="adulto" />
-        <Picker.Item label="66+" value="senior" />
-      </Picker>
+        <Text style={styles.text}>Password</Text>
 
-      <Text style={styles.text}>Email</Text>
-
-      <TextInput style={styles.input}
-        // onChangeText={onChangeNumber}
-        // value={number}
-        placeholder="maria@gmail.com" />
-
-      <Text style={styles.text}>Password</Text>
-
-      <TextInput style={styles.input}
-        // onChangeText={onChangeNumber}
-        // value={number}
-        secureTextEntry={true}
-        placeholder="********" />
-
+        <TextInput style={styles.input}
+          // onChangeText={onChangeNumber}
+          // value={number}
+          secureTextEntry={true}
+          placeholder="********" />
+      </View>
       <Pressable style={styles.saveButton}>
-        <Text>Save changes
+        <Text style={styles.buttonText}>Save changes
         </Text>
       </Pressable>
 
@@ -68,50 +75,60 @@ const styles = StyleSheet.create({
     backgroundColor: '#EBEBEB',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: 18,
-    fontFamily: 'Inter',
-    fontWeight: '400',
-    width: 220,
+  formContainer: {
+    width: 230,
     marginLeft: 'auto',
     marginRight: 'auto',
+  },
+  text: {
+    fontSize: 18,
+    // fontFamily: 'Inter',
+    fontWeight: '400',
+    width: '100%',
     marginBottom: 8,
   },
   input: {
     fontSize: 15,
-    fontFamily: 'Inter',
+    // fontFamily: 'Inter',
     fontWeight: '400',
     backgroundColor: 'white',
     height: 31,
-    width: 220,
+    width: '100%',
     borderRadius: 8,
-    marginLeft: 'auto',
-    marginRight: 'auto',
     marginBottom: 15,
     paddingLeft: 10,
+  },
+  dropdownContainer: {
+    borderRadius: 10,
+    backgroundColor: 'white',
+    height: 40,
+    overflow: 'hidden',
+    width: 120,
+    justifyContent: 'center',
+    paddingVertical: 8,
+    marginBottom: 15,
+    fontSize: 2,
+    // paddingBottom: 10,
   },
   dropdown: {
     fontSize: 15,
-    fontFamily: 'Inter',
-    fontWeight: '400',
-    backgroundColor: 'white',
-    borderColor: 'transparent',
-    height: 31,
-    width: 220,
-    borderRadius: 8,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginBottom: 15,
-    paddingLeft: 10,
-    dropdownIconRippleColor: '#1D5F98'
+    // // fontFamily: 'Inter',
+    // fontWeight: '400',
+    // backgroundColor: 'blue',
+    height: 20,
+    width: 120,
   },
   userImgContainer: {
-    // borderColor: 'red',
-    // borderWidth: 3,
-    alignItems: 'center',
     height: 140,
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
-
+  imagem: {
+    width: 117,
+    height: 117,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
   cameraIcon: {
     backgroundColor: '#1D5F98',
     color: 'white',
@@ -120,12 +137,11 @@ const styles = StyleSheet.create({
     paddingLeft: 2,
     borderRadius: 4,
     position: 'absolute',
-    marginTop: 85,
-    marginLeft: 90,
+    bottom: 1,
+    right: 6,
   },
   saveButton: {
     backgroundColor: '#1D5F98',
-    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 'auto',
@@ -134,8 +150,11 @@ const styles = StyleSheet.create({
     width: 151,
     height: 30,
     borderRadius: 26,
+  },
+  buttonText: {
     // fontFamily: 'Inter',
     fontWeight: '400',
     fontSize: 15,
+    color: 'white',
   }
 });
